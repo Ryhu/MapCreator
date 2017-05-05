e45f5aeac776f7018785f4b461b1b51f7f6498e1
ryan <ryanhuuu@gmail.com>
2017-05-05 16:01:47 -0400

    map done, click tile to change tile

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -11,9 +11,8 @@ angular.module('appyApp')
   .controller('AboutCtrl', function () {
 
     var tileArray = [];
-    var arrayX = 5;
-    var arrayY = 5;
-
+    var arrayX = 10;
+    var arrayY = 10;
     for (var i=0;i<arrayX;i++){
       var tempArray = [];
       for (var j=0;j<arrayY;j++){
@@ -22,8 +21,33 @@ angular.module('appyApp')
       tileArray.push(tempArray);
     }
 
+    this.grass ='pics/grass.png';
+
+
+
 
 
     this.tiles = tileArray;
     this.numba = 5;
+
+    this.increment = function(index,index2){
+      this.tiles[index][index2]++;
+    };
+
+
+    this.tilePicker = function(index,index2){
+      var tile = this.tiles[index][index2]%3;
+      if (tile == 0){
+        return 'pics/grass.png';
+      }
+      else if (tile == 1){
+        return 'pics/forest.png';
+      }
+      else if (tile == 2){
+        return 'pics/mountain.png';
+      };
+    };
+
+
+
   });
