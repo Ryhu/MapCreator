9dbf3b70f8248e8f37dc5c1aec85ede6504bf016
ryan <ryanhuuu@gmail.com>
2017-05-10 13:56:04 -0400

    figured out how to put controller in directive, clean up before testing

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,8 +8,24 @@
  * Controller of the appyApp
  */
 angular.module('appyApp')
-  .controller('MainCtrl', function () {
+.directive('mySharedScope', function () {
+  return {
+      controller:"MainCtrl as main",
+      template: 'Name: {{main.mouse}}<br /> Street: {{main.grass}}',
+      link: function ($scope, element, attrs, ctrl) {
 
+          element.on('mousedown', function () {
+
+              element.css('background-color', 'yellow');
+          });
+          element.on('mouseup', function () {
+              console.log(ctrl);
+              element.css('background-color', 'white');
+          });
+      }
+  };
+})
+  .controller('MainCtrl', function () {
         var tileArray = [];
         var arrayX = 15;
         var arrayY = 15;
@@ -26,11 +42,30 @@ angular.module('appyApp')
         this.numba = 5;
         this.selected = 'pics/grass.png';
         this.selectedID = 0;
+
+        this.mouse = "ghh";
+
+
         this.tileSet = {
           0: 'pics/grass.png',
           1: 'pics/forest.png',
-          2: 'pics/mountain.png'
+          2: 'pics/mountain.png',
+          3: 'pics/riverH.png',
+          4: 'pics/riverV.png',
+          5: 'pics/roadV.png',
+          6: 'pics/roadH.png'
         };
+        this.buttonCodes = {
+          81: 0,
+          87: 1,
+          69: 2,
+          82: 3,
+          65: 4,
+          83: 5,
+          68: 6,
+
+
+        }
         this.key = "a";
         this.select = function(num){
           $("#tile"+ this.selectedID).removeClass("selected");
@@ -48,21 +83,13 @@ angular.module('appyApp')
         };
 
         this.buttonPress = function(event){
-          this.numba++;
-          console.log(event);
-          if (event.keyCode == 81){
-            this.select(0);
-          }
-          else if (event.keyCode == 87){
-            this.select(1);
-          }
-          else if (event.keyCode == 69){
-            this.select(2);
-          }
-
+          console.log(event.keyCode);
+          this.select(this.buttonCodes[event.keyCode]);
         };
 
-
+        this.addy = function(){
+          this.numba++;
+        }
 
 
 
